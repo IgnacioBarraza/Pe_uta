f5e72cfb22f1d7384dc9a85312e77372dd63bcc2
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2023-11-08 20:14:39 -0300

    feat: first part of the login form

--- src/pages/accessPage/components/login.tsx
+++ src/pages/accessPage/components/login.tsx
@@ -1,6 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "./login.css";
+import { useForm } from "react-hook-form";
+
+
+type FormData = {
+  email: string,
+  password: string
+}
 
 export default function LogIn() {
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  }
+
   return (
     <>
       <div className="flex justify-center login">
@@ -12,13 +32,29 @@ export default function LogIn() {
                 alt="Uta logo"
                 className="uta-icon p-2"
               />
-              <span className="">
-                Feria de ciencias “Triunfando en el conocimiento” Universidad de
-                Tarapacá
-              </span>
-              <img src="src/assets/feria_de_ciencias_logo.svg" alt="Feria de ciencias logo" className="feria-icon p-1"/>
+              <div className="title-text text-white pt-2 pl-5 pr-5">
+                <span>Feria de ciencias</span>
+                <span>“Triunfando en el conocimiento”</span>
+                <span>Universidad de Tarapacá</span>
+              </div>
+              <img
+                src="src/assets/feria_de_ciencias_logo.svg"
+                alt="Feria de ciencias logo"
+                className="feria-icon p-1"
+              />
             </div>
-            <div className="form-container"></div>
+            <div className="form-container">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="rut-input flex flex-col m-auto p-10">
+                  <label htmlFor="rut">Rut</label>
+                  <input type="text" name="rut-input" id="rut" className=""/>
+                </div>
+                <div className="password-input flex flex-col m-auto">
+                  <label htmlFor="pass">Contraseña</label>
+                  <input type="password" name="pass-input" id="pass" />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
