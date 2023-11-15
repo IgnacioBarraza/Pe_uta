3106730d2162af2cbaae6903117bee9598af4375
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2023-11-14 23:14:34 -0300

    feat: finish the navbar, add responsive to the navbar

--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBookmark,
@@ -8,58 +9,64 @@ import {
 import { faSquareCheck } from "@fortawesome/free-regular-svg-icons";
 import { Link } from "react-router-dom";
 import "./navbar.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const iconSize = "xl";
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  const handleResize = () => {
+    if (window.innerWidth >= 1280 && isMenuOpen) {
+      setIsMenuOpen(false);
+    }
   };
+  // Add resize event listener
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isMenuOpen]);
 
   return (
     <>
       <div className="navbar flex justify-between items-center p-4">
         <div className="left-container flex items-center justify-center">
           <div className="nav-btns flex items-center">
-            <button className="xl:hidden" onClick={toggleMenu}>
-              <FontAwesomeIcon icon={faBars} size={iconSize} />
-            </button>
-            <div
-              className={`hidden xl:flex items-center space-x-2 ${
-                isMenuOpen ? "flex-col absolute bg-white" : ""
-              }`}
-            >
-              <Link to="/home">
-                <button className="nav-btn flex items-center justify-center">
-                  <FontAwesomeIcon icon={faHome} size={iconSize} />
-                  <span className="font-bold text-lg lg:ml-3 ml-1">Inicio</span>
-                </button>
-              </Link>
-              <Link to="/home/dashboard">
-                <button className="nav-btn ml-1 lg:ml-2 flex items-center justify-center">
-                  <FontAwesomeIcon icon={faBookmark} size={iconSize} />
-                  <span className="font-bold text-lg lg:ml-3 ml-1">
-                    Proyectos
-                  </span>
-                </button>
-              </Link>
-              <Link to="/evaluated">
-                <button className="nav-btn ml-1 lg:ml-2 flex items-center justify-center">
-                  <FontAwesomeIcon icon={faSquareCheck} size={iconSize} />
-                  <span className="font-bold text-lg lg:ml-3 ml-1">
-                    Evaluados
-                  </span>
-                </button>
-              </Link>
-            </div>
+          <button className="xl:hidden" onClick={toggleMenu}>
+            <FontAwesomeIcon icon={faBars} size={iconSize} />
+          </button>
+          <div className={`xl:flex items-center ${isMenuOpen ? "flex-col absolute top-24 left-1 hamburguer-menu" : "hidden"}`}>
+            <Link to="/home">
+            {/* nav-btn flex items-center justify-center */}
+              <button className={`xl:flex items-center ${isMenuOpen ? "flex items-start menu-nav-btn mb-2" : "nav-btn flex items-center justify-center"}`}>
+                <FontAwesomeIcon icon={faHome} size={iconSize} />
+                <span className="font-semibold text-lg lg:ml-3 ml-1">Inicio</span>
+              </button>
+            </Link>
+            <Link to="/home/dashboard">
+              <button className={`xl:flex items-center ${isMenuOpen ? "flex items-start menu-nav-btn mb-2" : "nav-btn flex items-center justify-center"}`}>
+                <FontAwesomeIcon icon={faBookmark} size={iconSize} />
+                <span className="font-semibold text-lg lg:ml-3 ml-1">Proyectos</span>
+              </button>
+            </Link>
+            <Link to="/evaluated">
+            {/* ml-1 lg:ml-2 */}
+              <button className={`xl:flex items-center ${isMenuOpen ? "flex items-start menu-nav-btn" : "nav-btn flex items-center justify-center"}`}>
+                <FontAwesomeIcon icon={faSquareCheck} size={iconSize} />
+                <span className="font-semibold text-lg lg:ml-3 ml-1">Evaluados</span>
+              </button>
+            </Link>
           </div>
         </div>
+        </div>
         <div className="center-container flex justify-center items-center">
-          <div className="title-text text-black pt-2 pl-5 pr-5 flex items-center justify-center h-12">
-            <span>Feria de ciencias “Triunfando en el conocimiento”</span>
-            <span>Universidad de Tarapacá</span>
+          <div className="title-text text-black flex items-center justify-center h-12">
+            <span className="font-semibold title">Feria de ciencias “Triunfando en el conocimiento”</span>
+            <span className="font-semibold title">Universidad de Tarapacá</span>
           </div>
         </div>
         <div className="right-container flex justify-end">
