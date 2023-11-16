5a2fecbe369c92122517aa2481e58460e10be1ff
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2023-11-16 00:22:56 -0300

    feat: first part of home page component

---
+++ src/components/Footer/footer.tsx
@@ -0,0 +1,9 @@
+import './footer.css'
+
+export default function Footer() {
+  return (
+    <>
+      <div className="w-full footer-container h-20"></div>
+    </>
+  )
+}
--- src/pages/HomePage/homepage.tsx
+++ src/pages/HomePage/homepage.tsx
@@ -1,12 +1,29 @@
-import './homepage.css';
-import Navbar  from '../../components/NavBar/navbar';
+import "./homepage.css";
+import Navbar from "../../components/NavBar/navbar";
+import Footer from "../../components/Footer/footer";
 
 export default function HomePage() {
+  const rut = localStorage.getItem("userRut");
 
-  return(
+  return (
     <>
-    <Navbar />
-    <h1>Home Page</h1>
+      <div className="fixed left-0 right-0 z-10">
+        <Navbar />
+      </div>
+        <div className="page-container bg-stone-100">
+          <div className="home-container bg-stone-400">
+            <div className="welcome flex justify-center items-center ">
+              <span className="font-semibold text-2xl">
+                ¡Bienvenido/a, {rut} 👋
+              </span>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      <div className="fixed left-0 right-0 bottom-0">
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
