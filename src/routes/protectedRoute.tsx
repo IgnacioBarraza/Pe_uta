0e374034f8b374dec449c3e3c284dce87badc40b
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2023-11-24 03:27:41 -0300

    feat: add protected routes

---
+++ src/routes/protectedRoute.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  roles?: string[];
+}
+
+function ProtectedRoute({ roles }: ProtectedRouteProps) {
+  const navigate = useNavigate();
+  const [userRole, setUserRole] = useState<string | null>(null);
+  const isAuthenticated = localStorage.getItem("token");
+  const userId = localStorage.getItem("userId");
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/");
+    } 
+  }, [isAuthenticated, userId, roles, userRole, navigate]);
+
+  return <Outlet />;
+}
+
+export default ProtectedRoute;
--- src/routes/routesConfig.tsx
+++ src/routes/routesConfig.tsx
@@ -4,6 +4,7 @@ import ProjectsBySubject from '../pages/ProjectsBySubject/projectsbysubject';
 import ProjectsEvaluated from '../pages/ProjectsEvaluated/projectsevaluated';
 import EvaluateProject from '../pages/EvaluateProject/evaluateprojects';
 import LogIn from '../pages/accessPage/login';
+import ProtectedRoute from './protectedRoute';
 
 const routes = [
   {
@@ -15,26 +16,36 @@ const routes = [
     path: '/home',
     component: <HomePage />,
     exact: true,
+    protection: <ProtectedRoute
+    roles={["user", "admin"]} />
   },
   {
     path: '/projects',
     component: <Projects />,
     exact: true,
+    protection: <ProtectedRoute
+    roles={["user", "admin"]} />
   },
   {
     path: '/projects_by_subject/:id',
     component: <ProjectsBySubject />,
     exact: true,
+    protection: <ProtectedRoute
+    roles={["user", "admin"]} />
   },
   {
     path: '/project_id/:id',
     component: <EvaluateProject />,
     exact: true,
+    protection: <ProtectedRoute
+    roles={["user", "admin"]} />
   },
   {
     path: '/evaluated',
     component: <ProjectsEvaluated />,
     exact: true,
+    protection: <ProtectedRoute
+    roles={["user", "admin"]} />
   },
 ];
 
