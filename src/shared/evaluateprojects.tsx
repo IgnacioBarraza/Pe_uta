aaff08079d674e7279a8fba7c1bb68e79fc5760d
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-09-11 19:14:13 -0300

    🔥 delete styles files

--- src/shared/evaluateprojects.tsx
+++ src/shared/evaluateprojects.tsx
@@ -1,9 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import "../styles/evaluateproject.css";
 import Formulario from "../components/formulario";
 
 export default function EvaluateProject() {
