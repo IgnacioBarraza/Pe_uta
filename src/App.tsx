47d02ad59308d8d1482e1040a9ac6c146028ad7f
Ignacio Barraza Rioja <ignacio.barraza.rioja@gmail.com>
2024-10-25 00:18:19 -0300

    ♻️ Refactor code of app file to router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,8 @@
-import Footer from "@/components/Footer/footer";
-import Navbar from "@/components/NavBar/navbar";
-import { Toaster } from "@/components/ui/toaster";
-import { AuthProvider } from "@/providers/authProvider";
-import { BackendProvider } from "@/providers/backendContext";
-import { PropsDataProvider } from "@/providers/propsContext";
 import Router from "@/router/router";
 import routes from "@/routes/routesConfig";
-import { FirebaseProvider } from "./providers/firebaseProvider";
-import { DataProvider } from "./providers/dataProvider";
 
 function App() {
-  return (
-    <AuthProvider>
-      <FirebaseProvider>
-        <BackendProvider>
-          <DataProvider>
-            <PropsDataProvider>
-              <div className="flex flex-col min-h-[100dvh]">
-                <Navbar />
-                <main className="flex-1">
-                  <Router routes={routes} />
-                  <Toaster />
-                </main>
-                <Footer />
-              </div>
-            </PropsDataProvider>
-          </DataProvider>
-        </BackendProvider>
-      </FirebaseProvider>
-    </AuthProvider>
-  );
+  return <Router routes={routes} />
 }
 
 export default App;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,38 +1,62 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Page404 } from "../shared/page404";
-import ProtectedRoute from "../routes/protectedRoute";
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Page404 } from '../shared/page404'
+import ProtectedRoute from '../routes/protectedRoute'
+import Navbar from '@/components/NavBar/navbar'
+import Footer from '@/components/Footer/footer'
+import { Toaster } from '@/components/ui/toaster'
+import { FirebaseProvider } from '@/providers/firebaseProvider'
+import { AuthProvider } from '@/providers/authProvider'
+import { BackendProvider } from '@/providers/backendContext'
+import { DataProvider } from '@/providers/dataProvider'
+import { PropsDataProvider } from '@/providers/propsContext'
+import { LocationProvider } from '@/providers/locationContext'
 
 export default function Router(props) {
-    // Function to recursively render routes
-    const renderRoutes = (routes) => {
-        return routes.map((route) => {
-            // Check if route requires protection
-            const element = route.protection ? (
-                <ProtectedRoute roles={route.protection.roles}>
-                    {route.component}
-                </ProtectedRoute>
-            ) : (
-                route.component
-            );
+  // Function to recursively render routes
+  const renderRoutes = (routes) => {
+    return routes.map((route) => {
+      // Check if route requires protection
+      const element = route.protection ? (
+        <ProtectedRoute roles={route.protection.roles}>
+          {route.component}
+        </ProtectedRoute>
+      ) : (
+        route.component
+      )
 
-            return (
-                <Route
-                    key={route.path}
-                    path={route.path}
-                    element={element}
-                >
-                    {route.routes && renderRoutes(route.routes)}
-                </Route>
-            );
-        });
-    };
+      return (
+        <Route key={route.path} path={route.path} element={element}>
+          {route.routes && renderRoutes(route.routes)}
+        </Route>
+      )
+    })
+  }
 
-    return (
-        <BrowserRouter>
-            <Routes>
-                {renderRoutes(props.routes)}
-                <Route path="*" element={<Page404 />} />
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <LocationProvider>
+        <AuthProvider>
+          <FirebaseProvider>
+            <BackendProvider>
+              <DataProvider>
+                <PropsDataProvider>
+                  <div className="flex flex-col min-h-[100dvh]">
+                    <Navbar />
+                    <main className="flex-1">
+                      <Routes>
+                        {renderRoutes(props.routes)}
+                        <Route path="*" element={<Page404 />} />
+                      </Routes>
+                      <Toaster />
+                    </main>
+                    <Footer />
+                  </div>
+                </PropsDataProvider>
+              </DataProvider>
+            </BackendProvider>
+          </FirebaseProvider>
+        </AuthProvider>
+      </LocationProvider>
+    </BrowserRouter>
+  )
 }
